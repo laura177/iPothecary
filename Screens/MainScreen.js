958199ca47ba16e61d54a8520fec5dd3f8e79230
Laura Buffington <laura.c.buffington@gmail.com>
2021-08-30 10:08:12 -0700

    initiated tab navigation

---
+++ Screens/MainScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, ScrollView, View, SafeAreaView, TouchableOpacity } from 'react-native';
+
+class MainScreen extends React.Component {
+    render(){
+        return (
+            <SafeAreaView>
+                <Text>Hello World</Text>
+            </SafeAreaView>
+        )
+    }
+}
+
+export default MainScreen

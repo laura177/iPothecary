62032b351f7d528256c5c0c868a73fd06a864640
Laura Buffington <laura.c.buffington@gmail.com>
2021-08-30 18:30:19 -0700

    re configure package-loxk.json and modules

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Text, ScrollView, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
+/*
+Main Screen is the page where user can view the scheduled reminders listed from soonest to latest
+Displays the
+*/
+
 class MainScreen extends React.Component {
     render(){
         return (
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import MainScreen from './Screens/MainScreen';
+import Profile from './Screens/Profile';
+import Manager from './Screens/Manager';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
